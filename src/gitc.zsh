3686374a4dbfaf4a311975c7337d4705fcbfc29e
MrurBo <mrurbo2011@gmail.com>
2026-07-24 21:34:22 +0100

    Added darkreader support

--- src/gitc.zsh
+++ src/gitc.zsh
@@ -123,6 +123,7 @@ page_head() {
   echo "<html>"
   echo "<head>"
   echo "<meta charset=\"utf-8\">"
+  echo "<meta name=\"color-scheme\" content=\"dark\""
   echo "<style>"
   echo "$(<${ROOT_DIR}style.css)"
   echo "</style>"
